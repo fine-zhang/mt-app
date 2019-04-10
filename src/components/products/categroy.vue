ae74db18dc115d0c33db994eea1a7c7f67ddb876
fine-zhang <846922022@qq.com>
2019-04-10 20:32:39 +0800

    first commit

---
+++ src/components/products/categroy.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="m-product-categroy">
+    <dl class="classic">
+      <dt>分类</dt>
+      <dt>全部</dt>
+      <dd v-for="(item) in classicList" :key="item.type">
+        <m-select :name="item.title" :list="item.subList"/>
+      </dd>
+    </dl>
+    <dl class="classic">
+      <dt>区域</dt>
+      <dt>全部</dt>
+      <dd v-for="(item) in areaList" :key="item.type">
+        <m-select :name="item.title" :list="item.subList"/>
+      </dd>
+    </dl>
+  </div>
+</template>
+<script>
+import MSelect from "./select.vue";
+import api from '@/api/index.js'
+export default {
+  data() {
+    return {
+      classicList: [],
+      areaList: [{
+
+        "title": "推荐商圈",
+
+        "subList": [{
+
+            "name": "望京",
+
+            "id": 120000
+
+        }, {
+
+            "name": "昌平",
+
+            "id": 12222
+
+        }]
+
+    }]
+    };
+  },created() {
+    api.getClassify().then(res => {
+      this.classicList = res.data.data
+    })
+  },
+  components: {
+    MSelect
+  }
+};
+</script>
